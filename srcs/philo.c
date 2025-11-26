6c0f0087593154cc9699d93e79616728bea64612
tbhuiyan <tbhuiyan@student.42.fr>
2025-11-26 12:58:08 +0100

    ajout du main. test A FAIRE

--- srcs/philo.c
+++ srcs/philo.c
@@ -10,3 +10,23 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../includes/philo.h"
+
+int	int main(int ac, char **av)
+{
+	t_data	data;
+	t_data	*philos;
+
+	if (!parse_args(ac, av, &data))
+		return (1);
+	if (!init_data(&data))
+		return (1);
+	if (!init_philos(&philos, &data))
+		return (cleanup(NULL, &data), 1);
+	if (!create_threads(philos, &data))
+		return (cleanup(philos, &data), 1);
+	wait_threads(philos, &data);
+	cleanup(philos, &data);
+	return (0);
+}
+
