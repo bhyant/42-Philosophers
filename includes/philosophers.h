0a2fd4e8d22fd855b657f94d54b0836133a5082f
Taj Bhuiyan <tbhuiyan@f1br8s20.clusters.42paris.fr>
2025-11-26 08:15:10 +0100

    ajout de la struct globale et de la structuration des fichiers du projets.

---
+++ includes/philosophers.h
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tbhuiyan <tbhuiyan@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/26 07:33:05 by tbhuiyan          #+#    #+#             */
+/*   Updated: 2025/11/26 08:00:33 by tbhuiyan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSOPHERS_H
+# define PHILOSOPHERS_H
+
+#include <pthread.h>
+#include <sys/time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+typedef struct s_data
+{
+	int				nb_philo;
+	int				time_to_die;
+	int				time_to_eat;
+	int				time_to_sleep;
+	int				must_eat_count;
+	long			start_time;
+	int				someone_died;
+	int				all_ate_enough;
+	pthread_mutex_t	*forks;
+	pthread_mutex_t	print_mutex;
+	pthread_mutex_t	death_mutex;
+	pthread_mutex_t	meal_mutex;
+}	t_data;
+
+typedef struct s_philo
+{
+	int				id;
+	pthread_t		thread;
+	long			last_meal_time;
+	int				meals_eaten;
+	int				left_fork;
+	int				right_fork;
+	t_data			*data;
+}   t_philo;
+
+#endif
